ed39e54e4716805ade3943f9843da83bc140278e
Kuchar-J <Kuchar.Jakub.2zs@gmail.com>
2026-02-26 13:12:17 +0100

    VyresenePlusNecoExtra

---
+++ 02-collections/hokuspokus.py
@@ -0,0 +1,6 @@
+
+from collections import defaultdict
+listi = list(range(10))
+print(listi[-1])
+slovnik: defaultdict[str,list[list]]= defaultdict(lambda:["ahoj"])
+slovnik["matematika"].append(object/"martin")
